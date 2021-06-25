81f52a709e26ea9a8543c9f96da644e5464bf9ad
Kennedy Marino <knd.marino@gmail.com>
2021-06-25 18:32:50 -0300

    Criado Link com Firebase, verificaçao de senha

--- app/src/main/java/com/example/login/Cadastro.java
+++ app/src/main/java/com/example/login/Cadastro.java
@@ -1,12 +1,7 @@
 package com.example.login;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-
 import android.content.Intent;
 import android.os.Bundle;
-
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
@@ -14,18 +9,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.login.data.model.UserModel;
 import com.example.login.ui.login.LoginActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.auth.GetTokenResult;
 
 public class Cadastro extends AppCompatActivity {
+
     private Button btn_cad;
     private EditText nome, telefone, senha1, senha2, email;
     private FirebaseAuth mAuth;
@@ -35,7 +31,6 @@ public class Cadastro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
-
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -57,30 +52,59 @@ public class Cadastro extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                String registerEmail = email.getText().toString();
+                recuperarDados();
+
                 String senha = senha1.getText().toString();
-                String senha3 = senha2.getText().toString();
+                String confSenha = senha2.getText().toString();
 
-                if (senha.equals(senha3)) {
-                    mAuth.createUserWithEmailAndPassword(registerEmail, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                if(senha.equals(confSenha)){
+                    criarLogin();
 
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                abrirTelaPrincipal();
-                            } else {
-                                String error = task.getException().getMessage();
-                                Toast.makeText(Cadastro.this, "" + error, Toast.LENGTH_SHORT).show();
-                            }
+                }else{
 
-                        }
-                    });
-
-                } else {
-
-                    Toast.makeText(Cadastro.this, "A senha deve ser a mesma em ambos os campos", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(Cadastro.this, "As senhas não coincidem", Toast.LENGTH_SHORT).show();
 
                 }
+
+            }
+        });
+    }
+
+    private void recuperarDados(){
+
+        if (nome.getText().toString()==""||telefone.getText().toString()==""||email.getText().toString()==""||senha1.getText().toString()==""||senha2.getText().toString()=="") {
+
+            Toast.makeText(Cadastro.this, "Voce deve preencher todos os campos", Toast.LENGTH_SHORT).show();
+
+        } else {
+
+            u = new UserModel();
+            u.setUsername(nome.getText().toString());
+            u.setPassword(senha1.getText().toString());
+            u.setEmail(email.getText().toString());
+            u.setTelefone(telefone.getText().toString());
+
+        }
+
+    }
+
+    private void criarLogin(){
+
+        mAuth.createUserWithEmailAndPassword(u.getEmail(),u.getPassword()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    u.setId(user.getUid());
+                    u.salvar();
+
+                    abrirTelaPrincipal();
+
+                }else{
+
+                    Toast.makeText(Cadastro.this, "Erro ao criar Login", Toast.LENGTH_SHORT);
+                }
+
             }
         });
     }
@@ -89,6 +113,8 @@ public class Cadastro extends AppCompatActivity {
         Intent it = new Intent(Cadastro.this, LoginActivity.class);
         startActivity(it);
     }
+
+
 
 
     private TextWatcher cadText = new TextWatcher() {
@@ -106,24 +132,20 @@ public class Cadastro extends AppCompatActivity {
             String userSenha1 = senha1.getText().toString().trim();
             String userSenha2 = senha2.getText().toString().trim();
 
+
             btn_cad.setEnabled(!userNome.isEmpty() && !userTel.isEmpty() && !userEmail.isEmpty() && !userSenha1.isEmpty() && !userSenha2.isEmpty());
 
-        }
+
+            }
 
         @Override
         public void afterTextChanged(Editable s) {
-
-
         }
 
 
     };
 
-    private boolean isPasswordValid(String password) {
-        return password != null && password.trim().length() > 5;
 
-
-    }
 }
 
 
--- app/src/main/java/com/example/login/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/login/ui/login/LoginActivity.java
@@ -1,84 +1,98 @@
 package com.example.login.ui.login;
 
-import android.app.Activity;
-
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.annotation.StringRes;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.login.Cadastro;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.login.Cadastro;
 import com.example.login.R;
+import com.example.login.data.model.UserModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private LoginViewModel loginViewModel;
+
+    public EditText usernameEditText, passwordEditText;
+    public UserModel u;
+    public FirebaseAuth mAuth;
+    public Button loginButton, registerButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        loginViewModel = new ViewModelProvider(this, new LoginViewModelFactory())
-                .get(LoginViewModel.class);
 
-        final EditText usernameEditText = findViewById(R.id.username);
-        final EditText passwordEditText = findViewById(R.id.password);
-        final Button loginButton = findViewById(R.id.login);
-        final Button registerButton = findViewById(R.id.btn_registrar);
-        final ProgressBar loadingProgressBar = findViewById(R.id.loading);
 
-        loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
+        mAuth = FirebaseAuth.getInstance();
+
+        usernameEditText = findViewById(R.id.username);
+        passwordEditText = findViewById(R.id.password);
+
+        loginButton = findViewById(R.id.login);
+        registerButton = findViewById(R.id.btn_registrar);
+
+
+        usernameEditText.addTextChangedListener(afterTextChangedListener);
+        passwordEditText.addTextChangedListener(afterTextChangedListener);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable LoginFormState loginFormState) {
-                if (loginFormState == null) {
-                    return;
-                }
-                loginButton.setEnabled(loginFormState.isDataValid());
-                if (loginFormState.getUsernameError() != null) {
-                    usernameEditText.setError(getString(loginFormState.getUsernameError()));
-                }
-                if (loginFormState.getPasswordError() != null) {
-                    passwordEditText.setError(getString(loginFormState.getPasswordError()));
-                }
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, Cadastro.class));
+
             }
         });
-
-        loginViewModel.getLoginResult().observe(this, new Observer<LoginResult>() {
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable LoginResult loginResult) {
-                if (loginResult == null) {
-                    return;
-                }
-                loadingProgressBar.setVisibility(View.GONE);
-                if (loginResult.getError() != null) {
-                    showLoginFailed(loginResult.getError());
-                }
-                if (loginResult.getSuccess() != null) {
-                    updateUiWithUser(loginResult.getSuccess());
-                }
-                setResult(Activity.RESULT_OK);
-
-                //Complete and destroy login activity once successful
-                finish();
+            public void onClick(View v) {
+                receberDados();
+                logar();
             }
         });
+    }
+
+        private void logar () {
+            mAuth.signInWithEmailAndPassword(u.getEmail(), u.getPassword())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                startActivity(new Intent(LoginActivity.this, TelaPrincipal.class));
+
+                            } else {
+                                // If sign in fails, display a message to the user.
+
+                                Toast.makeText(LoginActivity.this, "Autenticação falhou",
+                                        Toast.LENGTH_SHORT).show();
+
+                            }
+                        }
+                    });
+        }
+
+        private void receberDados () {
+            u = new UserModel();
+            u.setEmail(usernameEditText.getText().toString());
+            u.setPassword(passwordEditText.getText().toString());
+
+
+        }
+
 
         TextWatcher afterTextChangedListener = new TextWatcher() {
             @Override
@@ -88,60 +102,25 @@ public class LoginActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                // ignore
+
             }
 
             @Override
             public void afterTextChanged(Editable s) {
-                loginViewModel.loginDataChanged(usernameEditText.getText().toString(),
-                        passwordEditText.getText().toString());
+                String user = usernameEditText.getText().toString().trim();
+                String senha = passwordEditText.getText().toString().trim();
+
+                loginButton.setEnabled(!user.isEmpty() && !senha.isEmpty());
+
             }
         };
 
-        usernameEditText.addTextChangedListener(afterTextChangedListener);
-        passwordEditText.addTextChangedListener(afterTextChangedListener);
-        passwordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
 
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_DONE) {
-                    loginViewModel.login(usernameEditText.getText().toString(),
-                            passwordEditText.getText().toString());
-                }
-                return false;
-            }
-        });
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loadingProgressBar.setVisibility(View.VISIBLE);
-                loginViewModel.login(usernameEditText.getText().toString(),
-                        passwordEditText.getText().toString());
-            }
-        });
-
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent it = new Intent(LoginActivity.this, Cadastro.class);
-                startActivity(it);
-
-            }
-        });
 
     }
 
 
-        private void updateUiWithUser (LoggedInUserView model){
-            String welcome = getString(R.string.welcome) + model.getDisplayName();
-            // TODO : initiate successful logged in experience
-            Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
 
 
-        }
 
-        private void showLoginFailed (@StringRes Integer errorString){
-            Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
-        }
-    }
+
